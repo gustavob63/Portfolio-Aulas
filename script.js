604f1da5104b8c4bc2ca88e6f492efc0399ca2d6
Gustavo Barbosa <gustavobs071@gmail.com>
2025-09-18 18:33:00 -0300

    Primeiro commit - Versão inicial do site

---
+++ script.js
@@ -0,0 +1,20 @@
+/* Alertas */
+
+document.getElementById('botao_a').onclick = 
+function (){alert("Você clicou no Botão A")}
+
+document.getElementById('botao_b').onclick = 
+function (){alert("Você clicou no Botão B")}
+
+document.getElementById('botao_c').onclick = 
+function (){alert("Você clicou no Botão C")}
+
+/* Tela de Login */
+
+document.getElementById('botao_login').onclick = 
+function (){alert("Login efetuado com sucesso!")}
+
+/* Tela de Login Hospital */
+
+document.getElementById('botao_hospital').onclick = 
+function (){alert("Login efetuado com sucesso!")}
